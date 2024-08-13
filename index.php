6e3409b52fb7641987507699d5797d4d2e06646c
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-08-13 22:21:20 +0800

    PHP that I didn't know yet 1

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
 echo "Hello World!";
+
+
+// Referencing
+echo "<br/>";
+$x = 10;
+$y = &$x;
+$x = 100;
+echo $x, ', ', $y;
+
+// Constants
+echo "<br/>";
+define('STATUS_PAID', 'paid');
+echo STATUS_PAID;
+
+const STATUS_PAID_CONST = 'paid';
+echo STATUS_PAID_CONST;
+
+//Variable Variables
+echo "<br/>";
+$foo = 'bar';
+$$foo = 'baz';
+echo $foo, $bar;
+
